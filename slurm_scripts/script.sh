fa2daa54b0611b5430a961810113c0833cf94e88
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-04-09 15:22:29 +0200

    fix number of task in slurm script

--- slurm_scripts/script.sh
+++ slurm_scripts/script.sh
@@ -4,16 +4,17 @@
 #SBATCH --error=TravailGPU%j.err # fichier d’erreur (%j = job ID)
 #SBATCH --constraint=v100-16g # demander des GPU a 16 Go de RAM
 #SBATCH --nodes=1 # reserver 1 nœud
-#SBATCH --ntasks=4 # reserver 4 taches (ou processus)
+#SBATCH --ntasks=1 # reserver 4 taches (ou processus)
 #SBATCH --gres=gpu:4 # reserver 4 GPU
 #SBATCH --cpus-per-task=10 # reserver 10 CPU par tache (et memoire associee)
 #SBATCH --time=01:00:00 # temps maximal d’allocation "(HH:MM:SS)"
 #SBATCH --qos=qos_gpu-dev # QoS
 #SBATCH --hint=nomultithread # desactiver l’hyperthreading
-#SBATCH --account=xyz@v100 # comptabilite V100
+#SBATCH --account=sxq@v100 # comptabilite V100
 
 module purge # nettoyer les modules herites par defaut
 conda deactivate # desactiver les environnements herites par defaut
-module load venvs/venvDistillation
+module load anaconda-py3/2023.09
+conda activate ../venvs/venvDistillation
 set -x # activer l’echo des commandes
 srun python -u preprocess.py --dataset_path $DSDIR/SegmentAnything_1B --features_path $WORK/data/SAM_Features --device cuda:0 --sam_type vit_h --sam_ckpt $WORK/data/sam_vit_h_4b8939.pth
